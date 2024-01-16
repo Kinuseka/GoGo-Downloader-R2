fb97786581ba341eaa92c7c6e663338d72435ce9
Kinuseka <realkingseeker1089@gmail.com>
2024-01-16 22:05:06 +0800

    Add options source and fix unused config

--- Varstorage.py
+++ Varstorage.py
@@ -7,12 +7,14 @@ class Constants:
     config_default = {
             "Video": {
                 "quality": 'best',
-                "selection": 'auto'
+                "selection": 'auto',
+                "source": 'built-in'
             },
             "Network": {
                 "base": "https://gogoanime3",
                 "domain": "net",
-                "consumet": "https://api.consumet.org"
+                "consumet": None,
+                "consumet_video_server": 'vidstreaming'
             }
         }
 
@@ -23,6 +25,8 @@ class Configuration:
         self.data_template = Constants.config_default
         self.valid_video_quality = ["1080p","720p","480p","360p","best"]
         self.valid_video_mode = ["auto","manual"]
+        self.valid_video_source = ['built-in', 'consumet']
+        self.valid_consumet_video_server = ['vidstreaming', 'gogocdn', 'streamsb']
     def load(self):
         "Load, returns itself"
         with open(self.name, "r") as f:
@@ -33,6 +37,15 @@ class Configuration:
             raise ValueError(f"{self.video_quality_preference} is not a valid option")
         if self.video_quality_mode not in self.valid_video_mode:
             raise ValueError(f"{self.video_quality_mode} is not a valid option")
+        if self.video_source not in self.valid_video_source:
+            raise ValueError(f"{self.video_source} is not a valid option")
+        if self.video_source == self.valid_video_source[1]:
+            if not self.get_consumet_api:
+                raise ValueError(f"{self.valid_video_source[1]} is used but the required value is {self.get_consumet_api}")
+            if self.get_consumet_video_server not in self.valid_consumet_video_server:
+                raise ValueError(f"{self.get_consumet_video_server} is not a valid option")
+            
+
     def generate_config(self):
         with open(self.name, "w") as f:
             yaml.dump(self.data_template, f)
@@ -49,6 +62,10 @@ class Configuration:
         api = self.data["Network"]["consumet"]
         return api
     @property
+    def get_consumet_video_server(self):
+        vidserver = self.data["Network"]["consumet_video_server"]
+        return vidserver
+    @property
     def video_quality_search(self):
         return self.data["Video"]["enable"]
     @property
@@ -58,6 +75,9 @@ class Configuration:
     @property
     def video_quality_mode(self):
         return self.data["Video"]["selection"]
+    @property
+    def video_source(self):
+        return self.data['Video']['source']
 
 if __name__ == "__main__":
     config = Configuration()
